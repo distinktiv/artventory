7c21c06701c0d0771780ed7ba0666270bdc4752e
Sabine Pierre <sabine.pierre@gmail.com>
2017-02-08 00:11:27 -0500

    adding domain to user server

--- users/src/main/java/com/artventory/distinktiv/UsersApplication.java
+++ users/src/main/java/com/artventory/distinktiv/UsersApplication.java
@@ -3,6 +3,7 @@ package com.artventory.distinktiv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +11,10 @@ public class UsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
+	}
+
+	@RequestMapping("/greeting")
+	public String greeting(){
+		return "Hello User Account -  From Eureka client";
 	}
 }
---
+++ users/src/main/java/com/artventory/distinktiv/domaine/User.java
@@ -0,0 +1,12 @@
+package com.artventory.distinktiv.domaine;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.io.Serializable;
+
+/**
+ * Created by distinktiv on 2017-02-07.
+ */
+@EntityScan
+public class User implements Serializable{
+}
