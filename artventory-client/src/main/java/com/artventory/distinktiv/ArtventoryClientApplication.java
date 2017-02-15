40108d57e21c6207d52ec74521ba0090a27671f1
Sabine Pierre <sabine.pierre@gmail.com>
2017-02-14 22:18:50 -0500

    pluggin thymeleaf dependencies

--- artventory-client/src/main/java/com/artventory/distinktiv/ArtventoryClientApplication.java
+++ artventory-client/src/main/java/com/artventory/distinktiv/ArtventoryClientApplication.java
@@ -2,7 +2,11 @@ package com.artventory.distinktiv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,14 @@ public class ArtventoryClientApplication {
 		SpringApplication.run(ArtventoryClientApplication.class, args);
 	}
 
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer(){
+		return container ->{
+			ErrorPage errorPage401 = new ErrorPage(HttpStatus.UNAUTHORIZED,"/401.html");
+			ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND,"/404.html");
+			ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/500.html");
+
+			container.addErrorPages(errorPage401, errorPage404, errorPage500);
+		};
+	}
 }
