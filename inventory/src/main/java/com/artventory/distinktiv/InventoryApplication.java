74e32f08e4f07fcf4310923b9f02fc091e6b2a2d
Sabine Pierre <sabine.pierre@gmail.com>
2017-02-15 21:59:42 -0500

    Creating inventory service

--- inventory/src/main/java/com/artventory/distinktiv/InventoryApplication.java
+++ inventory/src/main/java/com/artventory/distinktiv/InventoryApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Created by distinktiv on 2017-02-15.
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UsersApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
-	}
+public class InventoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
 
 }
